4101ebc0f44f6b6fba6983cc76180f833294ae3a
Chris Grams <37988265+chrisagrams@users.noreply.github.com>
2024-11-26 10:35:29 -0600

    feat: Added /build-container endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
+import docker.errors
 from fastapi import FastAPI, File, Form, UploadFile, HTTPException, Depends
 from minio import Minio
 from io import BytesIO
 import docker
 import zipfile
+import tarfile
 import uuid
 from sqlalchemy.orm import Session
 from schema import Base, SessionLocal, engine, Submission
@@ -74,3 +76,46 @@ async def upload(
     db.refresh(new_submission)
 
     return {"file_key": file_key, "message": "File uploaded successfully"}
+
+
+@app.post("/build-container/{file_key}")
+async def build_container(file_key: str):
+    response = minio_client.get_object(BUCKET_NAME, file_key)
+    zip_data = BytesIO(response.read())  # Read in-memory
+    response.close()
+    response.release_conn()
+
+    with zipfile.ZipFile(zip_data) as z:
+        print(z.infolist())
+        if not any(info.filename.startswith("transform/") for info in z.infolist()):
+            # Check if transform directory is present
+            raise HTTPException(
+                status_code=400, detail="'transform' directory not found in zip."
+            )
+
+        # Create a tar archive for the "transform" directory
+        tar_data = BytesIO()
+        with tarfile.open(fileobj=tar_data, mode="w") as tar:
+            for file_info in z.infolist():
+                if file_info.filename.startswith("transform/"):
+                    file_bytes = z.read(file_info)
+                    tar_info = tarfile.TarInfo(
+                        name=file_info.filename[len("transform/") :]
+                    )
+                    tar_info.size = len(file_bytes)
+                    tar.addfile(tar_info, BytesIO(file_bytes))
+        tar_data.seek(0)
+
+    # Try to build Docker image
+    try:
+        image, logs = client.images.build(
+            fileobj=tar_data,
+            custom_context=True,
+            tag=f"transform-{file_key}:latest",
+        )
+        for log in logs:
+            print(log)  # TODO: Set up a websocket to emmit this to client
+    except docker.errors.BuildError as e:
+        raise HTTPException(status_code=500, detail=f"Docker build failed: {str(e)}")
+
+    return {"message": "Docker image built successfully", "image_id": image.id}
