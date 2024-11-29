78d3669a0ea86159728d748843a55e7e4a7e6f7b
Chris Grams <chrisagrams@gmail.com>
2024-11-29 17:38:36 -0600

    fix: Added bucket as parameter to process functions. More logging

--- backend/process.py
+++ backend/process.py
@@ -17,15 +17,16 @@ logger = logging.getLogger(__name__)
 INTER_RUN_BUCKET = "inter-run-bucket"
 
 
-def download_file(client: Minio, url: str, object_name: str):
+def download_file(client: Minio, url: str, bucket:str, object_name: str):
     try:
         # Ensure the bucket exists
-        if not client.bucket_exists(INTER_RUN_BUCKET):
-            client.make_bucket(INTER_RUN_BUCKET)
+        if not client.bucket_exists(bucket):
+            client.make_bucket(bucket)
+            logger.info(f"{bucket} created.")
 
         # Check if the file already exists
         try:
-            client.stat_object(INTER_RUN_BUCKET, object_name)
+            client.stat_object(bucket, object_name)
             logger.info(
                 f"{object_name} already exists in the bucket. Skipping download."
             )
@@ -33,15 +34,16 @@ def download_file(client: Minio, url: str, object_name: str):
         except S3Error as e:
             if e.code != "NoSuchKey":
                 raise
-
+        
+        # Download URL and put into bucket
         with requests.get(url) as response:
             response.raise_for_status()  # Raise exception for HTTP errors
             with tempfile.NamedTemporaryFile(delete=True, dir="/tmp") as tmp_file:
                 tmp_file.write(response.content)
                 tmp_file.seek(0)
                 client.put_object(
-                    bucket_name=INTER_RUN_BUCKET,
-                    object_name=object_name,
+                    bucket_name=bucket,
+                    object_name=f"download/{object_name}",
                     data=tmp_file,
                     length=len(response.content),
                     content_type=response.headers.get("content-type"),
@@ -55,15 +57,18 @@ def download_file(client: Minio, url: str, object_name: str):
 
 
 
-def deconstruct_file(client: Minio, object_name: str):
-    response = client.get_object(INTER_RUN_BUCKET, object_name)
+def deconstruct_file(client: Minio, bucket: str, object_name: str):
+    # Get mzML from bucket
+    response = client.get_object(bucket, f"download/{object_name}")
     file_data = response.read()
 
+    # Create two temporary directories, input and output
     with tempfile.TemporaryDirectory(dir="/tmp") as input_dir, tempfile.TemporaryDirectory(dir="/tmp") as output_dir:
         input_file_path = os.path.join(input_dir, object_name)
         with open(input_file_path, "wb") as input_file:
             input_file.write(file_data)
 
+        # Configure and start container
         container = docker_client.create_container(
             image="chrisagrams/mzml-construct:latest",
             command="python -u deconstruct.py /input/test.mzML /output/ -f npy",
@@ -84,16 +89,17 @@ def deconstruct_file(client: Minio, object_name: str):
             with open(output_file_path, "rb") as output_file:
                 file_stat = os.stat(output_file_path)
                 client.put_object(
-                    INTER_RUN_BUCKET,
+                    bucket,
                     f"deconstruct/{file_name}",
                     data=output_file,
                     length=file_stat.st_size
                 )
+                logger.info(f"Created deconstruct/{file_name}.")
 
 
 def prepare_benchmarks(client: Minio, url: str, object_name: str):
-    download_file(client, url, object_name)
-    deconstruct_file(client, object_name)
+    download_file(client, url, INTER_RUN_BUCKET, object_name)
+    deconstruct_file(client, INTER_RUN_BUCKET, object_name)
 
 
 # def benchmark_image(tag: str):
