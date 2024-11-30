8254edd22058758c8b413662c94cc17578cd3caa
Chris Grams <chrisagrams@gmail.com>
2024-11-29 18:22:33 -0600

    feat: Added search_file

--- backend/process.py
+++ backend/process.py
@@ -1,11 +1,9 @@
 import docker
 import logging
 import requests
-import tempfile
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 import os
-import tarfile
-import time
-from io import BytesIO
+import zipfile
 from minio import Minio
 from minio.error import S3Error
 
@@ -38,7 +36,7 @@ def download_file(client: Minio, url: str, bucket:str, object_name: str):
         # Download URL and put into bucket
         with requests.get(url) as response:
             response.raise_for_status()  # Raise exception for HTTP errors
-            with tempfile.NamedTemporaryFile(delete=True, dir="/tmp") as tmp_file:
+            with NamedTemporaryFile(delete=True, dir="/tmp") as tmp_file:
                 tmp_file.write(response.content)
                 tmp_file.seek(0)
                 client.put_object(
@@ -56,6 +54,19 @@ def download_file(client: Minio, url: str, bucket:str, object_name: str):
         logger.error(f"HTTP request error: {e}")
 
 
+def put_directory_to_minio(client: Minio, bucket: str, prefix: str, output_dir: str):
+    for file_name in os.listdir(output_dir):
+        output_file_path = os.path.join(output_dir, file_name)
+        with open(output_file_path, "rb") as output_file:
+            file_stat = os.stat(output_file_path)
+            client.put_object(
+                bucket,
+                f"{prefix}/{file_name}",
+                data=output_file,
+                length=file_stat.st_size
+            )
+            logger.info(f"Created {prefix}/{file_name}.")
+
 
 def deconstruct_file(client: Minio, bucket: str, object_name: str):
     # Check if deconstruct folder exists and if .xml and .npy files are present
@@ -71,7 +82,7 @@ def deconstruct_file(client: Minio, bucket: str, object_name: str):
     file_data = response.read()
 
     # Create two temporary directories, input and output
-    with tempfile.TemporaryDirectory(dir="/tmp") as input_dir, tempfile.TemporaryDirectory(dir="/tmp") as output_dir:
+    with TemporaryDirectory(dir="/tmp") as input_dir, TemporaryDirectory(dir="/tmp") as output_dir:
         input_file_path = os.path.join(input_dir, object_name)
         with open(input_file_path, "wb") as input_file:
             input_file.write(file_data)
@@ -92,22 +103,52 @@ def deconstruct_file(client: Minio, bucket: str, object_name: str):
         docker_client.wait(container=container.get('Id'))
 
         # Upload results to MinIO
+        put_directory_to_minio(client, bucket, "deconstruct", output_dir)
+
+
+def search_file(client: Minio, bucket: str, object_name: str):
+    # Get mzML from bucket
+    response = client.get_object(bucket, f"download/{object_name}")
+    file_data = response.read()
+
+    # Create two temporary directories, input and output
+    with TemporaryDirectory(dir="/tmp") as input_dir, TemporaryDirectory(dir="/tmp") as output_dir:
+        input_file_path = os.path.join(input_dir, object_name)
+        with open(input_file_path, "wb") as input_file:
+            input_file.write(file_data)
+
+        # Configure and start container
+        container = docker_client.create_container(
+            image="chrisagrams/msfragger:UP000005640",
+            entrypoint="/app/entrypoint.sh",
+            command="/input/test.mzML /output",
+            host_config=docker_client.create_host_config(
+                binds = {
+                    input_dir: {'bind': '/input', 'mode': 'ro'},
+                    output_dir: {'bind': '/output', 'mode': 'rw'}
+                }
+            )
+        )
+
+        docker_client.start(container=container.get("Id"))
+        docker_client.wait(container=container.get("Id"))
+
+        # Unzip the output ZIP file
         for file_name in os.listdir(output_dir):
-            output_file_path = os.path.join(output_dir, file_name)
-            with open(output_file_path, "rb") as output_file:
-                file_stat = os.stat(output_file_path)
-                client.put_object(
-                    bucket,
-                    f"deconstruct/{file_name}",
-                    data=output_file,
-                    length=file_stat.st_size
-                )
-                logger.info(f"Created deconstruct/{file_name}.")
+            if file_name.endswith('.zip'):
+                zip_path = os.path.join(output_dir, file_name)
+                with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                    zip_ref.extractall(output_dir)
+                os.remove(zip_path)
+
+        # Upload results to MinIO
+        put_directory_to_minio(client, bucket, "search", output_dir)
 
 
 def prepare_benchmarks(client: Minio, url: str, object_name: str):
     download_file(client, url, INTER_RUN_BUCKET, object_name)
     deconstruct_file(client, INTER_RUN_BUCKET, object_name)
+    search_file(client, INTER_RUN_BUCKET, object_name)
 
 
 # def benchmark_image(tag: str):
