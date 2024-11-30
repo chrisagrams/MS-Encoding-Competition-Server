bf2cd8f2860785ffc6f86160b902f01752d7724b
Chris Grams <37988265+chrisagrams@users.noreply.github.com>
2024-11-30 13:21:58 -0600

    feat: Added BrowserRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import { SubmissionForm } from './components/Form'
-import { Ranking } from './components/Ranking'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import { SubmissionForm } from './components/Form';
+import { Ranking } from './components/Ranking';
 
 function App() {
   return (
-    <>
-      {/* <SubmissionForm></SubmissionForm> */}
-      <Ranking></Ranking>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/submit" element={<SubmissionForm />} />
+        <Route path="/ranking" element={<Ranking />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
