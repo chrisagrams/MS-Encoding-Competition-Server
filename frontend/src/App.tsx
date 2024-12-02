6284b547c73913f6510952d09bae4437369492f0
Chris Grams <37988265+chrisagrams@users.noreply.github.com>
2024-12-02 15:08:49 -0600

    feat: Added home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Home } from './components/Home'
 import { SubmissionForm } from './components/Form';
 import { Ranking } from './components/Ranking';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/submit" element={<SubmissionForm />} />
         <Route path="/ranking" element={<Ranking />} />
       </Routes>
---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,52 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { useNavigate } from "react-router-dom"
+import github from "@/assets/github.svg"
+
+export const Home = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <Card className="h-full">
+                <CardHeader>
+                    <CardTitle>MS Encoding Competition</CardTitle>
+                    <CardDescription>
+                        A competition set out to determine the best encoding/decoding methods for mass spectrometry data.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Button 
+                        variant="outline"
+                        onClick={() =>
+                            window.open("https://github.com/chrisagrams/MS-Encoding-Competition", "_blank", "noopener,noreferrer")
+                        }
+                    >
+                        <img className="w-4" src={github}></img>
+                        Code Repository
+                    </Button>
+                    <hr className="my-4"></hr>
+                    <div className="flex gap-4">
+                        <Button
+                            onClick={() => { navigate('/submit')}}
+                        >Make a Submission</Button>
+                        <Button
+                            onClick={() => { navigate('/ranking')}}
+                        >View Rankings</Button>
+                    </div>
+                
+                </CardContent>
+                <CardFooter>
+
+                </CardFooter>
+            </Card>
+        </>
+    )
+}
