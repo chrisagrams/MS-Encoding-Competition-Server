7a3e311ec9fe5e91b7a1df1831492bd5febb3d78
Chris Grams <37988265+chrisagrams@users.noreply.github.com>
2024-12-19 16:57:01 -0600

    feat: Added code viewer to submission view

--- backend/endpoints/results.py
+++ backend/endpoints/results.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from typing import List
 from sqlalchemy.orm import Session
 from models.models import ResultModel
 from models.schema import Submission, TestResult
 from utils.database import get_db
+from utils.minio import minio_client
+from io import BytesIO
+import zipfile
 
 router = APIRouter()
 
@@ -28,3 +31,44 @@ def get_all_results(db: Session = Depends(get_db)):
         )
 
     return result_list
+
+
+@router.get("/result", response_model=ResultModel)
+def get_result(id: str, db: Session = Depends(get_db)):
+    result = db.query(TestResult).join(Submission).filter(TestResult.submission_id == id).first()
+
+    if not result:
+        raise HTTPException(status_code=404, detail="Result not found")
+
+    submission = result.submission
+    return ResultModel(
+        submission_id=submission.file_key,
+        name=submission.name,
+        submission_name=submission.submission_name,
+        encoding_runtime=result.encoding_runtime,
+        decoding_runtime=result.decoding_runtime,
+        ratio=result.ratio,
+        accuracy=result.accuracy,
+        status=result.status,
+    )
+
+
+@router.get("/submission-source")
+def get_submission_source(id: str):
+    try:
+        response = minio_client.get_object("submission-uploads", id)
+        zip_data = BytesIO(response.read())
+        with zipfile.ZipFile(zip_data, 'r') as zip_file:
+            encode_content = zip_file.read("transform/encode.py").decode('utf-8')
+            decode_content = zip_file.read("transform/decode.py").decode('utf-8')
+        
+        return {
+            "encode.py": encode_content,
+            "decode.py": decode_content
+        }
+    except minio_client.exceptions.NoSuchKey:
+        raise HTTPException(status_code=404, detail="File not found in MinIO bucket")
+    except KeyError:
+        raise HTTPException(status_code=400, detail="Required files are missing in the zip")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An unexpected error occurred: {str(e)}")
--- frontend/src/components/Submission.tsx
+++ frontend/src/components/Submission.tsx
@@ -1,11 +1,160 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import hljs from "highlight.js";
+import "highlight.js/styles/github.css";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+import { Button } from "@/components/ui/button"
+import { ChevronsUpDown } from "lucide-react"
+import { Result } from "./columns"
+import { IoCheckmarkCircle, IoCloseCircle, IoTime } from "react-icons/io5"
+
 
 export const Submission = () => {
-    const { uuid } = useParams(); 
-    return (
-      <div>
-        <h1>Submission Page</h1>
-        <p>UUID: {uuid}</p>
-      </div>
-    );
-  }
+  const { uuid } = useParams();
+  const [files, setFiles] = useState<{ encode?: string; decode?: string }>({});
+  const [resultData, setResultData] = useState<Result>()
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      try {
+        const response = await fetch(`/api/submission-source?id=${uuid}`);
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+        const data = await response.json();
+        setFiles({ encode: data["encode.py"], decode: data["decode.py"] });
+      } catch (err: any) {
+        setError(err.message || "Failed to fetch files.");
+      }
+    };
+    fetchFiles();
+  }, [uuid]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`/api/result?id=${uuid}`); 
+            
+            if (!response.ok) {
+                throw new Error('Failed to get results.');
+            }
+
+            const resultData: Result = await response.json();
+            setResultData(resultData);
+        } catch (err) {
+            console.error('Error fetching data:', err);
+        } finally {
+        }
+    };
+    fetchData();
+    const intervalId = setInterval(fetchData, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const highlightCode = (code: string) => {
+    return { __html: hljs.highlight(code, { language: "python" }).value };
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Submission</CardTitle>
+        <CardDescription>{uuid}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {!error && (
+          <>
+            <ResizablePanelGroup direction="horizontal">
+              <ResizablePanel className="px-3">
+                <h3 className="text-xl font-bold">Code</h3>
+                <Collapsible>
+                  <CollapsibleTrigger asChild>
+                  <div className="flex row">
+                    <h3 className="my-auto cursor-pointer font-mono">encode.py</h3>
+                    <Button variant="ghost" size="sm">
+                      <ChevronsUpDown className="h-4 w-4" />
+                    </Button>
+                  </div>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-6 overflow-auto">
+                      <pre>
+                        <code
+                          dangerouslySetInnerHTML={
+                            files.encode ? highlightCode(files.encode) : undefined
+                          }
+                        ></code>
+                      </pre>
+                    </div>
+                  </CollapsibleContent>
+                </Collapsible>
+                <Collapsible>
+                <CollapsibleTrigger asChild>
+                  <div className="flex row">
+                    <h3 className="my-auto cursor-pointer font-mono">decode.py</h3>
+                    <Button variant="ghost" size="sm">
+                      <ChevronsUpDown className="h-4 w-4" />
+                    </Button>
+                  </div>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-6 overflow-auto">
+                      <pre>
+                        <code
+                          dangerouslySetInnerHTML={
+                            files.decode ? highlightCode(files.decode) : undefined
+                          }
+                        ></code>
+                      </pre>
+                    </div>
+                  </CollapsibleContent>
+                </Collapsible>
+              </ResizablePanel>
+              <ResizableHandle />
+              <ResizablePanel className="px-3">
+                <div className="flex justify-between">
+                  <h3 className="text-xl font-bold">Result</h3>
+                  <div className="flex gap-2">
+                    <p className="my-auto">Status: </p>
+                    {resultData?.status === "success" && (
+                        <IoCheckmarkCircle className="text-green-400 text-3xl" />
+                    )}
+                    {resultData?.status === "failed" && (
+                        <IoCloseCircle className="text-red-400 text-3xl" />
+                    )}
+                    {resultData?.status === "pending" && (
+                        <IoTime className="text-yellow-400 text-3xl" />
+                    )}
+                  </div>
+                </div>
+                <div>
+                  
+                </div>
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </>
+        )}
+      </CardContent>
+      <CardFooter></CardFooter>
+    </Card>
+  );
+};
