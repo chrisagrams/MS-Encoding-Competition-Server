c641a61b0f91aca0e50982845fbd3b96244082e2
Chris Grams <37988265+chrisagrams@users.noreply.github.com>
2025-01-07 15:37:03 -0600

    fix: Allow undefined data prop in IdentificationChart component

--- frontend/src/components/IdentificationChart.tsx
+++ frontend/src/components/IdentificationChart.tsx
@@ -24,7 +24,7 @@ const chartConfig = {
 } satisfies ChartConfig;
 
 type IdentificationChartProps = {
-  data: Result;
+  data: Result | undefined;
 };
 
 export const IdentificationChart: React.FC<IdentificationChartProps> = ({ data }) => {
